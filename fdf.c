074b96c64eab1753c708117143ee74c31cbe68d6
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2022-03-21 23:37:39 +0100

    FDF Update

--- fdf.c
+++ fdf.c
@@ -32,7 +32,7 @@ int draw(t_fdf *fdf)
 int main(int argc, char **argv)
 {
 	t_fdf 	*fdf;
-	
+	int	check;
 
 	if (argc == 2)
 	{
@@ -42,29 +42,16 @@ int main(int argc, char **argv)
 		fdf->img = (t_img *)malloc(sizeof(t_img));
 		if (!fdf->img)
 			return (1);
-		ft_parse(argv[1], fdf);
+		check = ft_parse(argv[1], fdf);
+		if (check < 0)
+			return (ERROR);
 		init_win(fdf);
 		draw(fdf);
 		mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, fdf->img->img_ptr, 0, 0);
-		// work on the line drawing algo , zoom , rotation for one frame , and then change other frames for other maps
-		//mlx_loop_hook(fdf->mlx_ptr, esc_press, &fdf);
 		mlx_hook(fdf->win_ptr, ON_KEYDOWN, 0, esc_press, &fdf);
 		mlx_loop(fdf->mlx_ptr);
 	}
 	else
-		ft_printf("not enough arguments! \n");
+		ft_printf("Usage: ./fdf map_name \n");
 	return (0);
 }
-
-
-// for (int i = 0; i < fdf->line_count; i++)
-// 		{
-// 			for (int j = 0; j < fdf->word_count ; j++)
-// 			{
-// 				if (fdf->map_values[i][j] < 9)
-// 					printf("%d  ",fdf->map_values[i][j]);
-// 				else
-// 					printf("%d ",fdf->map_values[i][j]);
-// 			}
-// 			printf("\n");
-// 		}
